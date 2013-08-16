af1fd96ebeeca0ee7b579d9cf1ead88a3c9a2ee4
Stephan Richter <github@srsoftware.de>
2013-08-16 10:18:48 +0200

    umfangreiche umstrukturierungen vorgenommen

---
+++ src/de/srsoftware/tools/Filefilter.java
@@ -0,0 +1,18 @@
+package de.srsoftware.tools;
+
+import java.io.File;
+import java.io.FileFilter;
+
+
+public class Filefilter implements FileFilter {
+	private String name=null;
+	public  Filefilter(String filename){
+		name=filename;
+	}
+
+	public boolean accept(File arg0) {
+		// TODO Auto-generated method stub
+		return arg0.getName().startsWith(name);
+	}
+
+}
